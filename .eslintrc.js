7aa41182528f570e7f2f411ad24428bbf104f845
Luca Ban <mesqueeb@gmail.com>
2020-07-21 10:39:04 +0900

    build: improve project setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,7 +14,10 @@ module.exports = {
     // Needed to make the parser take into account 'vue' files
     extraFileExtensions: ['.vue'],
     parser: '@typescript-eslint/parser',
-    project: resolve(__dirname, './tsconfig.json'),
+    project: [
+      resolve(__dirname, './tsconfig.json'),
+      resolve(__dirname, './packages/**/tsconfig.json'),
+    ],
     tsconfigRootDir: __dirname,
     ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
     sourceType: 'module', // Allows for the use of imports
