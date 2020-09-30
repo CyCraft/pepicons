3d08de196e37d879d111c1acd30ce558d6c2967d
Luca Ban <mesqueeb@gmail.com>
2020-09-30 09:27:38 +0900

    chore: build

--- packages/pepicons/src/pop/circle-filled.ts
+++ packages/pepicons/src/pop/circle-filled.ts
@@ -1,1 +1,1 @@
-export default `<svg xmlns="http://www.w3.org/2000/svg" style="" style="" style="" width="20" height="20" viewBox="0 0 20 20"><circle cx="10" cy="10" r="6" fill="currentColor"/></svg>`
+export default `<svg xmlns="http://www.w3.org/2000/svg" style="" style="" style="" style="" width="20" height="20" viewBox="0 0 20 20"><circle cx="10" cy="10" r="6" fill="currentColor"/></svg>`
--- packages/pepicons/types/src/pop/bookmark-filled.d.ts
+++ packages/pepicons/types/src/pop/bookmark-filled.d.ts
@@ -1,2 +1,2 @@
-declare const _default: "<svg xmlns=\"http://www.w3.org/2000/svg\" style=\"\" style=\"\" style=\"\" width=\"20\" height=\"20\" viewBox=\"0 0 20 20\"><path fill=\"currentColor\" fill-rule=\"evenodd\" stroke=\"currentColor\" stroke-linecap=\"round\" stroke-linejoin=\"round\" stroke-width=\"2\" d=\"M5.5 3h9v14L10 11.967 5.5 17z\"/></svg>";
+declare const _default: "<svg xmlns=\"http://www.w3.org/2000/svg\" style=\"\" style=\"\" style=\"\" style=\"\" width=\"20\" height=\"20\" viewBox=\"0 0 20 20\"><path fill=\"currentColor\" fill-rule=\"evenodd\" stroke=\"currentColor\" stroke-linecap=\"round\" stroke-linejoin=\"round\" stroke-width=\"2\" d=\"M5.5 3h9v14L10 11.967 5.5 17z\"/></svg>";
 export default _default;
