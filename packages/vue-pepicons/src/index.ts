eeb363e82e000e4beb8cafe1ed76dff8642ea05f
Luca Ban <mesqueeb@users.noreply.github.com>
2022-01-08 15:10:55 +0900

    extract props & stuck at vue-next/issues/4294

--- packages/vue-pepicons/src/index.ts
+++ packages/vue-pepicons/src/index.ts
@@ -1,5 +1,6 @@
 import Pepicon from './Pepicon.vue'
 
+export type { PepiconProps } from './PepiconProps'
 export { Pepicon }
 export type { PepiconName } from 'pepicons'
 export { pepiconArray } from 'pepicons'
