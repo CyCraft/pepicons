16e9a3ea300b1e334ebd4ef6940dc0d92367c7c4
Luca Ban <mesqueeb@gmail.com>
2022-02-06 15:46:04 +0900

    chore: delete unused stub

--- stub/types__react/index.d.ts
+++
@@ -1,8 +0,0 @@
-/**
- * This is a fix to make sure we can use the VSCode Volar extension alongside projects that have `@types/react` as dependency.
- *
- * Without this fix the Volar extension will give errors on `:class="something"`.
- *
- * @see https://github.com/johnsoncodehk/volar/discussions/592#discussioncomment-1763880
- */
-export {}
