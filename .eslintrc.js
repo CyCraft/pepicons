728b0bc7f0f191f6ea6651e8cfbe06e010f6bf3a
Luca Ban <mesqueeb@gmail.com>
2022-02-06 15:47:00 +0900

    build: update dependencies and setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,96 +1,41 @@
-const { resolve } = require('path')
-
 module.exports = {
-  // https://eslint.org/docs/user-guide/configuring#configuration-cascading-and-hierarchy
-  // This option interrupts the configuration hierarchy at this file
-  // Remove this if you have an higher level ESLint config file (it usually happens into a monorepos)
+  ignorePatterns: ['dist', 'node_modules'],
   root: true,
-
-  // https://eslint.vuejs.org/user-guide/#how-to-use-custom-parser
-  // Must use parserOptions instead of "parser" to allow vue-eslint-parser to keep working
-  // `parser: 'vue-eslint-parser'` is already included with any 'plugin:vue/**' config and should be omitted
-  parserOptions: {
-    // https://github.com/typescript-eslint/typescript-eslint/tree/master/packages/parser#configuration
-    // https://github.com/TypeStrong/fork-ts-checker-webpack-plugin#eslint
-    // Needed to make the parser take into account 'vue' files
-    extraFileExtensions: ['.vue'],
-    parser: '@typescript-eslint/parser',
-    project: [
-      resolve(__dirname, './tsconfig.json'),
-      resolve(__dirname, './packages/**/tsconfig.json'),
-    ],
-    tsconfigRootDir: __dirname,
-    ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
-    sourceType: 'module', // Allows for the use of imports
-  },
-
   env: {
-    browser: true,
+    'browser': true,
+    'es2021': true,
+    'node': true,
+    'vue/setup-compiler-macros': true, // required for `<script setup>`
   },
-
-  // Rules order is important, please avoid shuffling them
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+    ecmaVersion: 2022,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
   extends: [
-    // Base ESLint recommended rules
-    // 'eslint:recommended',
-
-    // https://github.com/typescript-eslint/typescript-eslint/tree/master/packages/eslint-plugin#usage
-    // ESLint typescript rules
+    'eslint:recommended',
+    'plugin:vue/base',
+    'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
-    // consider disabling this class of rules if linting takes too long
-    'plugin:@typescript-eslint/recommended-requiring-type-checking',
-
-    // Uncomment any of the lines below to choose desired strictness,
-    // but leave only one uncommented!
-    // See https://eslint.vuejs.org/rules/#available-rules
-    'plugin:vue/essential', // Priority A: Essential (Error Prevention)
-    // 'plugin:vue/strongly-recommended', // Priority B: Strongly Recommended (Improving Readability)
-    // 'plugin:vue/recommended', // Priority C: Recommended (Minimizing Arbitrary Choices and Cognitive Overhead)
-
-    // https://github.com/prettier/eslint-config-prettier#installation
-    // usage with Prettier, provided by 'eslint-config-prettier'.
+    'plugin:vue/vue3-recommended',
     'prettier',
   ],
-
-  plugins: [
-    // required to apply rules which need type information
-    '@typescript-eslint',
-
-    // https://eslint.vuejs.org/user-guide/#why-doesn-t-it-work-on-vue-file
-    // required to lint *.vue files
-    'vue',
-  ],
-
-  globals: {
-    ga: true, // Google Analytics
-    cordova: true,
-    __statics: true,
-    process: true,
-    Capacitor: true,
-    chrome: true,
-  },
-
   // add your custom rules here
   rules: {
-    'prefer-promise-reject-errors': 'off',
-
-    // TypeScript
-    'quotes': ['warn', 'single', { avoidEscape: true }],
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-unsafe-call': 'off',
-    '@typescript-eslint/no-unsafe-assignment': 'off',
-    '@typescript-eslint/no-unsafe-member-access': 'off',
-    'comma-dangle': ['error', 'always-multiline'],
-    'quotes': 'off',
-
-    // allow debugger during development only
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-
+    'vue/script-setup-uses-vars': 'error', // required for `<script setup>`
     '@typescript-eslint/no-unused-vars': 'off',
-    '@typescript-eslint/no-unnecessary-type-assertion': 'off',
-    '@typescript-eslint/no-unsafe-return': 'off',
-    '@typescript-eslint/restrict-template-expressions': 'off',
-    '@typescript-eslint/ban-ts-comment': 'off',
+    '@typescript-eslint/array-type': ['error', { default: 'array' }],
     '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-empty-function': 'off',
+    '@typescript-eslint/ban-ts-comment': 'warn',
+    'vue/component-tags-order': ['error', { order: ['script', 'template', 'style'] }],
+    'vue/attribute-hyphenation': ['error', 'never', { ignore: [] }],
+    'vue/v-on-event-hyphenation': ['error', 'never'],
+  },
+  globals: {
+    defineProps: 'readonly', // required for `<script setup>`
+    defineEmits: 'readonly', // required for `<script setup>`
   },
 }
--- vetur.config.js
+++
@@ -1,7 +0,0 @@
-/**
- * documentation: https://vuejs.github.io/vetur/reference/#example
- * @type {import('vls').VeturConfig}
- */
-module.exports = {
-  projects: ['./packages/pepicons.com', './packages/vue-pepicons'],
-}
