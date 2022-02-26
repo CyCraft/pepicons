416cd1ce6b7df9c7504a97ce0c47e867589b23e8
Andy <andy@andykarwoski.com>
2022-02-26 11:00:11 -0600

    helpers moved and quasar.variables.sass removed

---
+++ packages/pepicons.com-new/src/helpers/iconList.ts
@@ -0,0 +1,9 @@
+const svgs = require.context('pepicons/svg', true, /^\.\/.*(\.svg)$/).keys()
+
+export const iconList: {
+  pop: string[]
+  print: string[]
+} = {
+  pop: svgs.filter((path) => path.includes('pop/')),
+  print: svgs.filter((path) => path.includes('print/')),
+}
