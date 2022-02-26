1f203788dd6c045782740551ba2721b9be72cb4e
Luca Ban <mesqueeb@gmail.com>
2022-02-26 23:52:08 +0900

    setup vite

---
+++ packages/pepicons.com-new/src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { Pepicon } from '@pepicons/vue'
+</script>
+
+<template>
+  <Pepicon type="pop" name="airplane" />
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
