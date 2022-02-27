b73be8fc878e58178924e0dfb026211800557759
Andy <andy@andykarwoski.com>
2022-02-27 11:30:04 -0600

    Vue router, and inital pages/layout added

--- packages/pepicons.com-new/src/App.vue
+++ packages/pepicons.com-new/src/App.vue
@@ -1,18 +1,5 @@
-<script setup lang="ts">
-import { Pepicon } from '@pepicons/vue'
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <Pepicon type="pop" name="airplane" />
+  <router-view />
 </template>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ packages/pepicons.com-new/src/components/_template.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="my-component">something</div>
+</template>
+
+<style lang="sass">
+// .my-component
+</style>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'MyComponent',
+  props: {},
+  setup(props) {
+    return {}
+  },
+})
+</script>
