eb29eba2a60fb671e5dbc35b71c548b47a8e85c7
Luca Ban <mesqueeb@users.noreply.github.com>
2022-03-10 11:39:31 +0900

    feat: add sync import example

---
+++ packages/vue/src/components/examples/Sync import.vue
@@ -0,0 +1,42 @@
+<script lang="ts" setup>
+import { PopAirplane } from '../../../dist/index.es'
+import { PopBattery } from '../../../dist/index.es'
+import { PopBicycle } from '../../../dist/index.es'
+import { PopBook } from '../../../dist/index.es'
+import { PopCamera } from '../../../dist/index.es'
+import { PopCar } from '../../../dist/index.es'
+import { PrintAirplane } from '../../../dist/index.es'
+import { PrintBattery } from '../../../dist/index.es'
+import { PrintBicycle } from '../../../dist/index.es'
+import { PrintBook } from '../../../dist/index.es'
+import { PrintCamera } from '../../../dist/index.es'
+import { PrintCar } from '../../../dist/index.es'
+</script>
+
+<template>
+  <div class="example">
+    <PopAirplane />
+    <PopBattery />
+    <PopBicycle />
+    <PopBook />
+    <PopCamera />
+    <PopCar />
+    <PrintAirplane />
+    <PrintBattery />
+    <PrintBicycle />
+    <PrintBook />
+    <PrintCamera />
+    <PrintCar />
+  </div>
+</template>
+
+<style scoped>
+.example {
+  display: flex;
+  flex-wrap: wrap;
+}
+.example > * {
+  margin: 1rem;
+  color: mediumslateblue;
+}
+</style>
