7e7b2c132a6c96b949e27ab712b34751ff294b93
Luca Ban <mesqueeb@users.noreply.github.com>
2022-03-10 11:50:34 +0900

    docs: add size examples

---
+++ packages/vue/src/components/examples/Size — CSS.vue
@@ -0,0 +1,44 @@
+<script lang="ts" setup>
+import Pepicon from '../Pepicon.vue'
+</script>
+
+<template>
+  <div class="example">
+    <Pepicon name="airplane" class="xs" />
+    <Pepicon name="battery" class="sm" />
+    <Pepicon name="bicycle" class="md" />
+    <Pepicon name="book" class="lg" />
+    <Pepicon name="camera" class="xl" />
+    <Pepicon name="car" class="xxl" />
+  </div>
+</template>
+
+<style scoped>
+.example {
+  display: flex;
+  flex-wrap: wrap;
+}
+.example > * {
+  margin: 1rem;
+  color: mediumslateblue;
+}
+
+.xs {
+  width: 4px;
+}
+.sm {
+  width: 8px;
+}
+.md {
+  width: 12px;
+}
+.lg {
+  width: 16px;
+}
+.xl {
+  width: 18px;
+}
+.xxl {
+  width: 22px;
+}
+</style>
---
+++ packages/vue/src/components/examples/Size — prop.vue
@@ -0,0 +1,25 @@
+<script lang="ts" setup>
+import Pepicon from '../Pepicon.vue'
+</script>
+
+<template>
+  <div class="example">
+    <Pepicon name="airplane" size="xs" />
+    <Pepicon name="battery" size="sm" />
+    <Pepicon name="bicycle" size="md" />
+    <Pepicon name="book" size="lg" />
+    <Pepicon name="camera" size="xl" />
+    <Pepicon name="car" size="xxl" />
+  </div>
+</template>
+
+<style scoped>
+.example {
+  display: flex;
+  flex-wrap: wrap;
+}
+.example > * {
+  margin: 1rem;
+  color: mediumslateblue;
+}
+</style>
