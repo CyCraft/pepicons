3511900420ecccbecadc2524c048ea796d383579
mesqueeb <mesqueeb@users.noreply.github.com>
2022-07-01 17:45:02 +0900

    lint: update config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,5 +1,12 @@
 module.exports = {
-  ignorePatterns: ['dist', 'node_modules'],
+  ignorePatterns: [
+    'dist',
+    'node_modules',
+    'build',
+    '.eslintrc.cjs',
+    '.postcssrc.js',
+    '.babel.config.js',
+  ],
   root: true,
   env: {
     'browser': true,
@@ -16,23 +23,59 @@ module.exports = {
   plugins: ['@typescript-eslint'],
   extends: [
     'eslint:recommended',
-    'plugin:vue/base',
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:vue/base',
     'plugin:vue/vue3-recommended',
     'prettier',
   ],
   // add your custom rules here
   rules: {
     'vue/script-setup-uses-vars': 'error', // required for `<script setup>`
+    // other rules
     '@typescript-eslint/no-unused-vars': 'off',
     '@typescript-eslint/array-type': ['error', { default: 'array' }],
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-empty-function': 'off',
     '@typescript-eslint/ban-ts-comment': 'warn',
+    '@typescript-eslint/explicit-module-boundary-types': 'off', // temporary during transition phase
+    '@typescript-eslint/triple-slash-reference': 'off',
+    'no-irregular-whitespace': 'off',
+    'no-useless-escape': 'off',
     'vue/component-tags-order': ['error', { order: ['script', 'template', 'style'] }],
     'vue/attribute-hyphenation': ['error', 'never', { ignore: [] }],
     'vue/v-on-event-hyphenation': ['error', 'never'],
+    'vue/multi-word-component-names': 'off',
+    'vue/order-in-components': 'off', // off because we want to transition to setup anyway
+    'vue/require-default-prop': 'off', // off because it's ugly when defining with a TS only `defineProps<{}>()` syntax
+    'vue/v-slot-style': [
+      'error',
+      {
+        atComponent: 'shorthand',
+        default: 'shorthand',
+        named: 'shorthand',
+      },
+    ],
+    'vue/attributes-order': [
+      'error',
+      {
+        order: [
+          'DEFINITION', // e.g. 'is', 'v-is'
+          'LIST_RENDERING', // e.g. 'v-for item in items'
+          'CONDITIONALS', // e.g. 'v-if', 'v-else-if', 'v-else', 'v-show', 'v-cloak'
+          'RENDER_MODIFIERS', // e.g. 'v-once', 'v-pre'
+          'GLOBAL', // e.g. 'id'
+          'UNIQUE', // e.g. 'ref', 'key'
+          'TWO_WAY_BINDING', // e.g. 'v-model'
+          'OTHER_DIRECTIVES', // e.g. 'v-custom-directive'
+          'OTHER_ATTR', // e.g. 'custom-prop="foo"', 'v-bind:prop="foo"', ':prop="foo"'
+          'EVENTS', // e.g. '@click="functionCall"', 'v-on="event"'
+          'CONTENT', // e.g. 'v-text', 'v-html'
+          'SLOT', // e.g. 'v-slot', 'slot'.
+        ],
+        alphabetical: false,
+      },
+    ],
   },
   globals: {
     defineProps: 'readonly', // required for `<script setup>`
