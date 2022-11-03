7d65cbd0f00ab996a330ea195f2d608d431b4ed2
mesqueeb <mesqueeb@users.noreply.github.com>
2022-11-03 15:31:15 +0900

    fix: remove dist folder from git cache

--- packages/pepicons/dist/types/src/categories.d.ts
+++
@@ -1,6 +0,0 @@
-/** Do not edit! This file is auto-generated. */
-import { Pepicon } from './types';
-export declare const categories: string[];
-export declare const pepiconCategoryDic: {
-    [name in Pepicon]: string;
-};
--- packages/pepicons/dist/types/src/synonyms/ja.d.ts
+++
@@ -1,5 +0,0 @@
-/** This file is to be manually edited. */
-import { Pepicon } from '../types';
-export declare const synonyms: {
-    [name in Pepicon]: string[];
-};
