9f200568f9406929e66a6c01810ce895d8328732
mesqueeb <mesqueeb@users.noreply.github.com>
2022-11-22 00:26:01 +0900

    feat: modernise component

--- packages/pepicons.com/src/components/HtmlButton.vue
+++ packages/pepicons.com/src/components/HtmlButton.vue
@@ -1,20 +1,24 @@
 <script lang="ts" setup>
 import { Pepicon } from '@pepicons/vue'
-import { PropType } from 'vue'
-const emit = defineEmits(['update:modelValue'])
-const props = defineProps({
-  modelValue: { type: Boolean, default: false },
-  type: { type: String as PropType<'pop' | 'print' | undefined> },
-  color: { type: String },
-  stroke: { type: String },
-})
+
+const props = defineProps<{
+  modelValue: boolean
+  type: 'pop' | 'print'
+  color: string
+  stroke: string
+}>()
+
+const emit = defineEmits<{
+  (e: 'update:modelValue', payload: boolean): void
+}>()
+
 function toggle() {
   emit('update:modelValue', !props.modelValue)
 }
 </script>
 
 <template>
-  <button :class="`html-button ${modelValue ? '_expanded' : ''}`" @click="toggle">
+  <button :class="`html-button ${modelValue ? '_expanded' : ''}`" @click="() => toggle()">
     <Pepicon class="_icon" name="angle-left" :type="type" :color="color" :stroke="stroke" />
     <Pepicon class="_icon" name="angle-right" :type="type" :color="color" :stroke="stroke" />
   </button>
