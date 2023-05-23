84db9050a6113ba5843925c14918c6012aa76a15
Luca Ban <mesqueeb@gmail.com>
2023-05-23 11:43:22 +0900

    fix: tweak build script

--- packages/pepicons/rollup.config.js
+++ packages/pepicons/rollup.config.js
@@ -9,42 +9,30 @@ import pkg from './package.json' assert { type: 'json' }
 // Builds
 // ------------------------------------------------------------------------------------------
 /**
- * @param {{ input: string; output: { esm: string; cjs: string } }} options
+ * @param {{ input: string; output: { esm: { types: string; default: string }; cjs: { types: string; default: string } } }} options
  */
 function esbuildAndDts(options) {
   const { input, output } = options
 
-  if (!output.cjs.endsWith('.cjs')) throw new Error('output.cjs must end with .cjs')
-  if (!output.esm.endsWith('.js')) throw new Error('output.esm must end with .js')
+  if (!output.cjs.default.endsWith('.cjs')) throw new Error('output.cjs.default must end with .cjs')
+  if (!output.esm.default.endsWith('.js')) throw new Error('output.esm.default must end with .js')
+  if (!output.cjs.types.endsWith('.d.cts')) throw new Error('output.cjs.types must end with .d.cts')
+  if (!output.esm.types.endsWith('.d.ts')) throw new Error('output.esm.types must end with .d.ts')
 
   return [
     {
       input,
       output: [
-        { file: output.esm, format: 'esm', generatedCode: { constBindings: true } },
-        { file: output.cjs, format: 'cjs', generatedCode: { constBindings: true } },
+        { file: output.esm.default, format: 'esm', generatedCode: { constBindings: true } },
+        { file: output.cjs.default, format: 'cjs', generatedCode: { constBindings: true } },
       ],
-      plugins: [
-        esbuild({
-          sourceMap: false,
-          target: 'esnext',
-          loaders: { '.json': 'json' },
-        }),
-      ],
+      plugins: [esbuild({ sourceMap: false, target: 'esnext', loaders: { '.json': 'json' } })],
     },
     {
       input,
       output: [
-        {
-          file: output.esm.replace('.js', '.d.ts'),
-          format: 'esm',
-          generatedCode: { constBindings: true },
-        },
-        {
-          file: output.cjs.replace('.cjs', '.d.cts'),
-          format: 'cjs',
-          generatedCode: { constBindings: true },
-        },
+        { file: output.esm.types, format: 'esm', generatedCode: { constBindings: true } },
+        { file: output.cjs.types, format: 'cjs', generatedCode: { constBindings: true } },
       ],
       plugins: [dts()],
     },
@@ -55,29 +43,29 @@ export default [
   ...esbuildAndDts({
     input: 'src/index.ts',
     output: {
-      esm: pkg.exports['.'].import.default,
-      cjs: pkg.exports['.'].require.default,
+      esm: pkg.exports['.'].import,
+      cjs: pkg.exports['.'].require,
     },
   }),
   ...esbuildAndDts({
     input: 'src/icons/print/index.ts',
     output: {
-      esm: pkg.exports['./print'].import.default,
-      cjs: pkg.exports['./print'].require.default,
+      esm: pkg.exports['./print'].import,
+      cjs: pkg.exports['./print'].require,
     },
   }),
   ...esbuildAndDts({
     input: 'src/icons/pop/index.ts',
     output: {
-      esm: pkg.exports['./pop'].import.default,
-      cjs: pkg.exports['./pop'].require.default,
+      esm: pkg.exports['./pop'].import,
+      cjs: pkg.exports['./pop'].require,
     },
   }),
   ...esbuildAndDts({
     input: 'src/icons/pencil/index.ts',
     output: {
-      esm: pkg.exports['./pencil'].import.default,
-      cjs: pkg.exports['./pencil'].require.default,
+      esm: pkg.exports['./pencil'].import,
+      cjs: pkg.exports['./pencil'].require,
     },
   }),
 ]
